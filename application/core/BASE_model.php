8122ad21375c82ae61e1e8ad07e9616bae82937f
Nguyen Thanh Nam <namvipc@gmail.com>
2019-12-12 08:29:31 +0700

    add BASE_model

---
+++ application/core/BASE_model.php
@@ -0,0 +1,24 @@
+<?php  
+    class BASE_model extends CI_Model {
+        protected $table = '';
+
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function get_all() {
+            return $this->db->get($this->table)->result();
+        }
+
+        public function get_by_id($id) {
+            $this->db->select('*');
+            $this->db->where('id', $id);
+            return $this->db->get($this->table)->result();
+        }
+
+        public function delete($id) {
+            $this->db->where('id', $id);
+            $query = $this->db->delete($this->table);
+            return true;
+        }
+    }
