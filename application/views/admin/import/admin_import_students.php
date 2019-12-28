b7e68bc446a2d5cc1c8539c134dda3e50eef9bb9
nld-8832 <linhnhat.dang9976@gmail.com>
2019-12-28 13:14:49 +0700

    Fix filename bug

--- application/views/admin/import/admin_import_students.php
+++ application/views/admin/import/admin_import_students.php
@@ -155,17 +155,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </form> 
 </div>
 
-<script type="text/javascript">
-    //COMMENT: BUG: CHANGE .custom-file-label on uploading - đổi .custom-file-label thành filename sau khi upload
-    $('#inputGroupFile04').on('change', '.custom-file-input', function(event) {
-        $(this).next('.custom-file-label').html(event.target.files[0].name);
-    })
-    
-</script>
-
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $('.custom-file-input').on('change', function() {
+        let fileName = $(this).val().split('\\').pop(); 
+   		$(this).next('.custom-file-label').addClass("selected").html(fileName); 
+    })
+</script>
 </body>
 </html>
